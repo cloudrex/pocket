ce1a49b0e1e2a8e1ea07e04c7443fa0d22fe3bed
CloudRex <cloudrex@outlook.com>
2019-03-27 20:10:12 -0400

    Expand Store tests

--- src/test/tests/store.ts
+++ src/test/tests/store.ts
@@ -3,7 +3,7 @@ import Store from "../../core/store";
 import {IDbModel} from "../../core/model";
 
 const testModel: IDbModel = {
-    id: 0
+    id: 1
 };
 
 const store: Store = new Store();
@@ -15,13 +15,65 @@ export default class {
         Assert.equal(store.size, 0);
     }
 
-    @test("should add an item")
+    @test("should add a model")
     @target(Store.prototype.put)
     public put() {
-        store.put(testModel);
-        
-        // Assert.
+        Assert.equal(store.put(testModel), store);
+    }
+
+    @test("should determine if a model is contained")
+    @target(Store.prototype.has)
+    public has() {
         Assert.true(store.has(testModel.id));
+    }
+
+    @test("should retrieve a model from its id")
+    @target(Store.prototype.get)
+    public get() {
         Assert.equal(store.get(testModel.id)!, testModel);
+    }
+
+    @test("should overwrite existing models")
+    @target(Store.prototype.put)
+    public put_overwrite() {
+        Assert.equal(store.put(testModel), store);
+        Assert.equal(store.size, 1);
+        Assert.equal(store.get(testModel.id)!, testModel);
+    }
+
+    @test("should remove existing models")
+    @target(Store.prototype.remove)
+    public remove() {
+        Assert.true(store.remove(testModel.id));
+
+        // Re-populate for upcoming tests.
+        store.put(testModel);
+    }
+
+    @test("should find an item by its id")
+    @target(Store.prototype.find)
+    public find() {
+        Assert.equal(store.find({
+            id: testModel.id
+        }), testModel);
+    }
+
+    @test("should add multiple models")
+    @target(Store.prototype.putMany)
+    public putMany() {
+        const testModel2: IDbModel = {
+            id: 2
+        };
+
+        const testModel3: IDbModel = {
+            id: 3
+        };
+
+        Assert.equal(store.putMany([testModel2, testModel3]), store);
+        Assert.equal(store.size, 3);
+
+        // Assert second and third models.
+        Assert.equal(store.get(testModel2.id)!, testModel2);
+        Assert.equal(store.get(testModel3.id)!, testModel3);
     }
 }
